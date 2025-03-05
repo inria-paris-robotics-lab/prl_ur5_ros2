c4eed13de264cac794d2f3cfe62863acb13808bf
CARECCHIO Timothee <timothee.carecchio@inria.fr>
2025-03-05 15:56:34 +0100

    wsg integrate in workbench simulation

--- wsg50_simulation/launch/wsg50_controllers.launch.py
+++ wsg50_simulation/launch/wsg50_controllers.launch.py
@@ -7,31 +7,37 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+
+  wsg50_controller_file=PathJoinSubstitution([FindPackageShare('wsg50_simulation'),'controllers', 'wsg50_integrate.yaml'])
+    
+  right_wsg50_controller_spawner_gr = Node(
+      package='controller_manager',
+      executable='spawner',
+      arguments=['right_wsg_50_gr', '--param-file', wsg50_controller_file],
+  )
+
+  right_wsg50_controller_spawner_gl = Node(
+      package='controller_manager',
+      executable='spawner',
+      arguments=['right_wsg_50_gl', '--param-file', wsg50_controller_file],
+  )
+
+  left_wsg50_controller_spawner_gr = Node(
+      package='controller_manager',
+      executable='spawner',
+      arguments=['left_wsg_50_gr', '--param-file', wsg50_controller_file],
+  )
+
+  left_wsg50_controller_spawner_gl = Node(
+      package='controller_manager',
+      executable='spawner',
+      arguments=['left_wsg_50_gl', '--param-file', wsg50_controller_file],
+  )
+
+
   return LaunchDescription([
-
-    Node(
-      package='controller_manager',
-      executable='spawner',
-      output='both',
-      arguments=["joint_state_broadcaster"]
-
-    ),
-    # Node(
-    #   package='controller_manager',
-    #   executable='spawner',
-    #   output='both',
-    #   arguments=["position_controller"]
-    # ),
-    Node(
-      package='controller_manager',
-      executable='spawner',
-      output='both',
-      arguments=["wsg_50_gl"]
-    ),
-    Node(
-      package='controller_manager',
-      executable='spawner',
-      output='both',
-      arguments=["wsg_50_gr"]
-    ),
+    right_wsg50_controller_spawner_gr,
+    right_wsg50_controller_spawner_gl,
+    left_wsg50_controller_spawner_gr,
+    left_wsg50_controller_spawner_gl
   ])
