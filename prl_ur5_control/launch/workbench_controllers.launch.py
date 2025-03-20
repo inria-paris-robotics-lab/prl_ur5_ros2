e2ea7aa7b3c55af6d28d323a97eb3d8cfd076a14
CARECCHIO Timothee <timothee.carecchio@inria.fr>
2025-03-20 15:25:32 +0100

    Controller update and standardisation of launch file for simulated and real robot

--- prl_ur5_control/launch/workbench_controllers.launch.py
+++ prl_ur5_control/launch/workbench_controllers.launch.py
@@ -1,26 +1,96 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.parameter_descriptions import ParameterFile
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
+
+def controller_spawner(controllers, active=True):
+    inactive_flags = ["--inactive"] if not active else []
+    return Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "--controller-manager",
+            "/controller_manager",
+            "--controller-manager-timeout",
+            "10",
+        ]
+        + inactive_flags
+        + controllers,
+    )
+
+
+def launch_setup(context):
+    # Arguments 
+    headless_mode = LaunchConfiguration("headless_mode").perform(context)
+    activate_joint_controller = LaunchConfiguration("activate_joint_controller").perform(context)
+    initial_joint_controller = LaunchConfiguration("initial_joint_controller").perform(context)
+
+    # Convert string to list
+    controllers_to_activate = initial_joint_controller.split(",")
+
+    # Default active controllers
+    controllers_active = ["joint_state_broadcaster"]
+    # Default inactive controllers
+    controllers_inactive = [
+        "left_joint_trajectory_controller",
+        "right_joint_trajectory_controller",
+        "forward_velocity_controller",
+        "forward_position_controller",
+        "left_force_mode_controller",
+        "right_force_mode_controller",
+        "left_passthrough_trajectory_controller",
+        "right_passthrough_trajectory_controller",
+        "left_freedrive_mode_controller",
+        "right_freedrive_mode_controller",
+        "left_io_and_status_controller",
+        "right_io_and_status_controller",
+        "left_speed_scaling_state_broadcaster",
+        "right_speed_scaling_state_broadcaster",
+        "left_force_torque_sensor_broadcaster",
+        "right_force_torque_sensor_broadcaster",
+        "left_tcp_pose_broadcaster",
+        "right_tcp_pose_broadcaster",
+        "left_ur_configuration_controller",
+        "right_ur_configuration_controller",
+        "left_scaled_joint_trajectory_controller",
+        "right_scaled_joint_trajectory_controller",
+    ]
+    # Update active and inactive controllers with initial joint controller
+    if activate_joint_controller.lower() == "true":
+        for controller in controllers_to_activate:
+            if controller in controllers_inactive:
+                controllers_inactive.remove(controller)
+            controllers_active.append(controller)
+
+    controller_spawners = [
+        controller_spawner(controllers_active),
+        controller_spawner(controllers_inactive, active=False),
+    ]
+
+    return controller_spawners
+
+
 def generate_launch_description():
-  return LaunchDescription([
+    declared_arguments = [
+        DeclareLaunchArgument(
+            "headless_mode",
+            default_value="false",
+            description="Enable headless mode for robot control",
+        ),
+        DeclareLaunchArgument(
+            "activate_joint_controller",
+            default_value="true",
+            description="Activate loaded joint controller.",
+        ),
+        DeclareLaunchArgument(
+            "initial_joint_controller",
+            default_value="left_joint_trajectory_controller,right_joint_trajectory_controller",
+            description="Initially loaded robot controller (comma-separated). The controllers need to be loaded in the controller manager. (used if activate_joint_controller is true)",
+        ),
+    ]
 
-    Node(
-      package='controller_manager',
-      executable='spawner',
-      output='both',
-      arguments=["joint_state_broadcaster"]
-
-    ),
-    Node(
-      package='controller_manager',
-      executable='spawner',
-      output='both',
-      arguments=["forward_velocity_controller",
-            '--param-file', PathJoinSubstitution([FindPackageShare('prl_ur5_control'),'config', 'dual_ur_controller.yaml']),]
-    ),
-  ])
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
--- prl_ur5_gazebo/launch/start_gazebo_sim.launch.py
+++ prl_ur5_gazebo/launch/start_gazebo_sim.launch.py
@@ -13,13 +13,18 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 from launch_ros.parameter_descriptions import ParameterValue
-
+import yaml
+from pathlib import Path
 
 
 def generate_launch_description():
     # Launch Arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
+
+    # Load the configuration file
+    config_file = os.path.join(get_package_share_directory('prl_ur5_robot_configuration'), 'config', 'standart_setup.yaml')
+   
     description_file=PathJoinSubstitution([FindPackageShare('prl_ur5_description'),'urdf', 'workbench_ur5.urdf.xacro'])
     
     dual_controller_file=PathJoinSubstitution([FindPackageShare('prl_ur5_control'),'config', 'dual_ur_controller.yaml'])
@@ -82,17 +87,42 @@ def generate_launch_description():
         ]),
     )
     
-    # Spawner pour le contrôleur wsg50
+    # Spawner gripper controller
+    config_path = Path(config_file) 
+    with config_path.open('r') as setup_file:
+        config = yaml.safe_load(setup_file)
 
-    wsg50_controller_launch = IncludeLaunchDescription(
+    left_gripper_controller = config.get('left')['gripper_controller']
+    right_gripper_controller = config.get('right')['gripper_controller']
+
+    left_gripper_controller = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
-            FindPackageShare('wsg50_simulation'),
-            'launch',
-            'wsg50_controllers.launch.py',
+                FindPackageShare('prl_ur5_control'),
+                'launch',
+                'standart_setup_gripper_control.launch.py',
             ])
         ]),
+        launch_arguments=[
+            ('gripper_controller', left_gripper_controller),
+            ('prefix', 'left_'),
+        ],
     )
+    right_gripper_controller = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('prl_ur5_control'),
+                'launch',
+                'standart_setup_gripper_control.launch.py',
+            ])
+        ]),
+        launch_arguments=[
+            ('gripper_controller', right_gripper_controller),
+            ('prefix', 'right_'),
+        ],
+    )
+
+
 
 
     bridge = Node(
@@ -102,9 +132,6 @@ def generate_launch_description():
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
             "/left_force_torque@geometry_msgs/msg/Wrench[gz.msgs.Wrench",
             "/right_force_torque@geometry_msgs/msg/Wrench[gz.msgs.Wrench",
-            "/world/workbench_world/model/dual_ur/link/base_link/sensor/bravo_camera_color/image@sensor_msgs/msg/Image[gz.msgs.Image",
-            "/world/workbench_world/model/dual_ur/link/base_link/sensor/bravo_camera_color/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo",
-            "/world/workbench_world/model/dual_ur/link/base_link/sensor/bravo_camera_depth/depth_image/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
         ],
         output="screen",
         parameters=[{"use_sim_time": use_sim_time}],
@@ -130,7 +157,8 @@ def generate_launch_description():
             on_exit=[
                   LogInfo(msg="Spawning controllers"),
                     controller_launch,
-                    wsg50_controller_launch,
+                    left_gripper_controller,
+                    right_gripper_controller,
                         ],
             )
         ),
