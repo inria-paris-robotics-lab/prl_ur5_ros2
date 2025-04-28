4e13a22a7f60fa3e696743f695960fe2aa464ce2
CARECCHIO Timothee <timothee.carecchio@inria.fr>
2025-04-28 16:43:05 +0200

    small fix to avoid advertising

--- prl_ur5_control/launch/mantis_gripper_controllers.launch.py
+++ prl_ur5_control/launch/mantis_gripper_controllers.launch.py
@@ -41,7 +41,7 @@ def launch_setup(context):
             ],
         )
         controllers_to_start.append(weiss_controller)
-    elif not controller or controller.lower() == "none":
+    elif not controller or controller.lower() == "None":
         pass
     else:
         raise RuntimeError("Unknown gripper controller '{}'".format(controller))
