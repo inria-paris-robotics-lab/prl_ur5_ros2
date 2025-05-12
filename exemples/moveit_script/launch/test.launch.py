5bd0fbf9f8436e988209215ec5087d9abcb07f9f
CARECCHIO Timothee <timothee.carecchio@inria.fr>
2025-05-12 16:29:19 +0200

    add moveit exemples

---
+++ exemples/moveit_script/launch/test.launch.py
@@ -0,0 +1,43 @@
+"""
+A launch file for running the motion planning python api tutorial
+"""
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+from moveit_configs_utils import MoveItConfigsBuilder
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder(robot_name="ur", package_name="prl_ur5_moveit")
+        .robot_description(file_path="config/ur5.urdf.xacro")
+        
+        .moveit_cpp(
+            file_path=get_package_share_directory("prl_ur5_moveit")
+            + "/config/planner.yaml"
+        )
+        .to_moveit_configs()
+    )
+
+
+
+    moveit_py_node = Node(
+        name="moveit_cmd",
+        package="moveit_script",
+        executable="moveit_cmd",
+        output="both",
+        parameters=[moveit_config.to_dict(), {'use_sim_time': True}],
+
+    )
+
+    return LaunchDescription(
+        [
+            moveit_py_node,
+        ]
+    )
