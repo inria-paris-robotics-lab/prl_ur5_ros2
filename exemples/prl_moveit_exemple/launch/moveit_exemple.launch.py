896ab878da9fdd7faf02721f97217c6906f750c2
CARECCHIO Timothee <timothee.carecchio@inria.fr>
2025-05-15 16:55:29 +0200

    Update moveit_exemple.launch.py

--- exemples/prl_moveit_exemple/launch/moveit_exemple.launch.py
+++ exemples/prl_moveit_exemple/launch/moveit_exemple.launch.py
@@ -31,7 +31,7 @@ def launch_setup(context):
         package="prl_moveit_exemple",
         executable="moveit_cmd",
         output="both",
-        parameters=[moveit_config.to_dict(), {'use_sim_time': True}],
+        parameters=[moveit_config.to_dict(), {'use_sim_time': use_sim_time}],
 
     )
 
